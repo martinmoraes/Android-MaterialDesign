1795695de62dddc6657f1d44129963fe7fc49438
Martin Morães <martin.moraes@gmail.com>
2016-06-14 14:03:23 -0300

    NavigationView implementado sem o menu. Abrindo e fechando.

--- app/src/main/java/br/com/appviral/materialdesign/PrincipalActivity.java
+++ app/src/main/java/br/com/appviral/materialdesign/PrincipalActivity.java
@@ -1,8 +1,11 @@
 package br.com.appviral.materialdesign;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -22,16 +25,21 @@ public class PrincipalActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
 
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+
+     /*   tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.addTab(tabLayout.newTab().setText("Aba Um"));
         tabLayout.addTab(tabLayout.newTab().setText("Aba Dois"));
-        tabLayout.addTab(tabLayout.newTab().setText("Aba Três"));
+        tabLayout.addTab(tabLayout.newTab().setText("Aba Três"));*/
 
     }
 
 
     public void umaMensagem(View view) {
-        Log.d("MEUAPP", "passou pela umaMensagem(View view)");
         Snackbar.make(view, "O FAB foi clicado!!!!", Snackbar.LENGTH_LONG).setAction("Fechar", new View.OnClickListener() {
             @Override
             public void onClick(View v) {
